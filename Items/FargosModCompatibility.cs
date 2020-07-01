06609126af35a9cd5c17b6508555c0fa99f9019e
direwolf420 <amakejk1@web.de>
2020-07-01 13:31:46 +0200

    make fargo support more reliable

--- Items/FargosModCompatibility.cs
+++ Items/FargosModCompatibility.cs
@@ -6,15 +6,16 @@ using VipixToolBox.UI;
 
 namespace VipixToolBox.Items
 {
-  public class FargosModCompatibility : GlobalItem
-  {
-    public override void UpdateAccessory (Item item, Player player, bool hideVisual)
+    public class FargosModCompatibility : GlobalItem
     {
-      VipixToolBoxPlayer myPlayer = player.GetModPlayer<VipixToolBoxPlayer>();
-      if (item.Name == "World Shaper Soul")//feel free to create an item with this display name
-      {
-        myPlayer.fargoRange = 54;//range of normal tools with fargo mod
-      }
+        public override void UpdateAccessory(Item item, Player player, bool hideVisual)
+        {
+            VipixToolBoxPlayer myPlayer = player.GetModPlayer<VipixToolBoxPlayer>();
+            //https://github.com/Fargowilta/FargowiltasSouls/blob/master/Items/Accessories/Souls/WorldShaperSoul.cs
+            if (item.modItem?.GetType().Name == "WorldShaperSoul")
+            {
+                myPlayer.fargoRange = 54;//range of normal tools with fargo mod
+            }
+        }
     }
-  }
 }
